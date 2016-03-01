8e69380153bbb7656df4a2c88ee3cf8843b74c95
loveirobert <loveirobert@gmail.com>
2016-03-01 15:09:58 +0100

    fine tuned eslint for generators

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router() // eslint-disable-line new-cap
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Token Service' })
 });
 
 module.exports = router
--- test/test.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-import assert from 'assert'
-
-describe('Test', function () {
-  it('Should test', function (done) {
-    assert.equal(1, 1)
-    done()
-  })
-})
